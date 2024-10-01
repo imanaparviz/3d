77c065cbb4d2705840ff3f008e75c385eecf8150
imanaparviz <imanaparviz@gmail.com>
2024-10-01 13:24:19 +0200

    Beschreibung der Änderungen

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,22 @@
-import type { Metadata } from 'next'
-import localFont from 'next/font/local'
-import './globals.css'
-
-
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Next.js 14',
-  description: 'Next.js 14 Crash Course',
-}
-const Copeland = localFont({
-  src:'../public/font/Copeland.otf',
-})
+  title: "Next.js 14",
+  description: "Next.js 14 Crash Course",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`bg-slate-900 text-slate-100 text-2xl tracking-wide  ${Copeland.className}`}>
-        <main className=''>
-        {children}
-        </main>
+      <body className={`bg-slate-900 text-slate-100 text-2xl tracking-wide`}>
+        <main className="">{children}</main>
       </body>
     </html>
-  )
+  );
 }
